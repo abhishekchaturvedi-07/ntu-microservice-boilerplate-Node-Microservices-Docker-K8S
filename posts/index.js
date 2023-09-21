217410d465e34bd3c07d7336d8b60847221408a2
abhishekchaturvedi-07 <abhishekchaturvedi789@gmail.com>
2023-09-21 16:08:19 +0530

    axios cors express nodemon for post and comments

---
+++ posts/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+//Random id generator
+const { randomBytes } = require('crypto');
+//Body Parser for returning the json representation
+const bodyParser = require('body-parser')
+// app.use(bodyParser.json()) <- Cannot use app before initialization
+
+//Initialize the application
+const app = express()
+
+app.use(bodyParser.json()) //Use Here
+
+// Dummy Posts
+const posts = {}
+
+//Methods
+app.get('/posts', (req, res)=>{
+    res.send(posts)
+})
+app.post('/posts', (req, res) => {
+    const id = randomBytes(4).toString('hex')
+    const {title} = req.body
+    posts[id] = {
+        id:id,
+        title: title,
+    }
+    res.status(200).send(posts[id])
+})
+
+//Listening
+app.listen(4000, () => {
+    console.log('Server running on port 4000')
+})
