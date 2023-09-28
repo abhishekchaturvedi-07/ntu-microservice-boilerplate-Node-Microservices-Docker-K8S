ff7eea310a9e089667a25bd5749b3f0f470911f0
abhishekchaturvedi-07 <abhishekchaturvedi789@gmail.com>
2023-09-28 18:52:25 +0530

    K8S Modify MEthod 2 - Deployment must be latest tag, make update to code, build and push docker hub and restart

--- posts/index.js
+++ posts/index.js
@@ -45,6 +45,6 @@ app.post('/events', (req,res) => {
 
 //Listening
 app.listen(4000, () => {
-    console.log('MEthod 1 checking to update the K8S Deployment ')
+    console.log('MEthod 2 checking to update the K8S Deployment!!')
     console.log('Server running on port 4000')
 })
