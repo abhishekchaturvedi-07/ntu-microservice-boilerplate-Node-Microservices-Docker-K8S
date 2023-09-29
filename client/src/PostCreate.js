2de1bbfcbfdc34647ed9f2355a3866b8e60beeec
abhishekchaturvedi-07 <abhishekchaturvedi789@gmail.com>
2023-09-29 23:26:26 +0530

    deploy react app in cluster

--- client/src/PostCreate.js
+++ client/src/PostCreate.js
@@ -7,7 +7,7 @@ const PostCreate = () => {
     const handleSubmit = async (e) => {
        e.preventDefault();
        alert("Submitting...")
-       await axios.post('http://posts.com/posts', {title})
+       await axios.post('http://posts.com/posts/create', {title})
        setTitle('New Title')
     }
     return(
